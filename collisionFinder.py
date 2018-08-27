f39eded23c883600139641e0250b6523711d807d
Rachit Jain <rachit.jain34@yahoo.com>
2018-08-27 11:52:56 +0530

    Created colliding tester

---
+++ collisionFinder.py
@@ -0,0 +1,51 @@
+import numpy
+
+def midPoint(p1,p2):
+	x = (p1.x + p2.x)/2
+	y = (p1.y + p2.y)
+	return Point(x,y)
+
+def newList(list,start,end):
+	l = []
+	for i in xrange(start,end):
+		l.append(list[i])
+	return l
+
+def deistnceOfLineFromCircle(a,b,c,circle): # ax+by+c = 0
+	
+
+def collisionFinderCircularObject(p1,p2,data):
+	#y = mx + c.  ax + by + c = 0.
+	# Therefore a = m, b = -1.
+
+	m = (p2.y - p1.y)/(p2.x - p1.x)
+	a = m
+	b = -1
+
+	distances = []
+	for i in data:
+		d = 
+
+def collisionFinder(start,end,volume):
+	if(start == end):
+		return false
+		break
+	point = midPoint(start,end)
+
+	if(volume(point) == 1):  # This means it is colliding.
+		return true
+		break
+	else:
+		collisionFinder(start,point,volume)
+		collisionFinder(point,end,volume)
+
+def collisionFinderSpline(path,volume):
+	length = len(path)
+	if (volume(path[length/2]) == 1):
+		return true
+		break
+	else:
+		path1 = newList(path,0,length/2)
+		path2 = newList(path,(length/2),length)
+		collisionFinderSpline(path1,volume)
+		collisionFinderSpline(path2,volume)
