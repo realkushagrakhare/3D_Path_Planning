657b2dbc33f04e8a14acd664ff0f436492cb3708
Avi K <kushagrakhare@rocketmail.com>
2018-11-13 22:46:52 +0530

    RRT-* Dubins is implemented

---
+++ rrt-star_Dubins_New.py
@@ -0,0 +1,437 @@
+import math, sys, pygame, random
+from math import *
+from pygame import *
+import sys
+import random
+import math
+import copy
+import numpy as np
+import dubins_path_planning
+import matplotlib.pyplot as plt
+
+
+XDIM = 720
+YDIM = 500
+windowSize = [XDIM, YDIM]
+delta = 10.0
+GAME_LEVEL = 1
+GOAL_RADIUS = 10
+MIN_DISTANCE_TO_ADD = 1.0
+NUMNODES = 5000
+FPS = 1000
+pygame.init()
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode(windowSize)
+white = 255, 255, 255
+black = 25, 25, 25
+red = 255, 0, 0
+blue = 0, 128, 0
+green = 0, 0, 255
+cyan = 0,180,105
+
+show_animation = True
+STEP_SIZE = 2
+curvature = 10
+
+count = 0
+obs = []
+
+def dist(p1,p2):    #distance between two points
+    return sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]))
+
+def point_circle_collision(p1, p2, radius):
+    distance = dist(p1,p2)
+    if (distance <= radius):
+        return True
+    return False
+
+def step_from_to(p1,p2):
+    if dist(p1,p2) < delta:
+        return p2
+    else:
+        theta = atan2(p2[1]-p1[1],p2[0]-p1[0])
+        return p1[0] + delta*cos(theta), p1[1] + delta*sin(theta)
+
+def collides(p):    #check if point collides with the obstacle
+    return False
+
+
+def init_obstacles(configNum):  #initialized the obstacle
+    global obs
+    obs = []
+    obs.append((150,100,18))
+    obs.append((150,130,18))
+    obs.append((150,160,18))
+    obs.append((150,190,18))
+    obs.append((150,220,18))
+    obs.append((150,250,18))
+    obs.append((180,250,18))
+    obs.append((210,250,18))
+    obs.append((240,250,18))
+    obs.append((270,250,18))
+    obs.append((300,250,18))
+    obs.append((330,250,18))
+    obs.append((330,220,18))
+    obs.append((330,190,18))
+    obs.append((330,160,18))
+    obs.append((330,130,18))
+    obs.append((330,100,18))
+    obs.append((300,100,18))
+    obs.append((270,100,18))
+    obs.append((240,100,18))
+    obs.append((240,130,18))
+    obs.append((240,160,18))
+    obs.append((240,190,18))
+    for (ox, oy, size) in obs:
+            #plt.plot(ox, oy, "ok", ms=30 * size)
+            pygame.draw.circle(screen, black, (ox, oy), size)
+
+
+def reset():
+    global count
+    screen.fill(white)
+    init_obstacles(GAME_LEVEL)
+    count = 0
+
+class RRT():
+    """
+    Class for RRT Planning
+    """
+
+    def __init__(self, start, goal, obstacleList, randArea,
+                 goalSampleRate=10, maxIter=120):
+        """
+        Setting Parameter
+        start:Start Position [x,y]
+        goal:Goal Position [x,y]
+        obstacleList:obstacle Positions [[x,y,size],...]
+        randArea:Ramdom Samping Area [min,max]
+        """
+        self.start = Node(start[0], start[1], start[2])
+        self.end = Node(goal[0], goal[1], goal[2])
+        self.minrand = randArea[0]
+        self.maxrand = randArea[1]
+        self.goalSampleRate = goalSampleRate
+        self.maxIter = maxIter
+        self.obstacleList = obstacleList
+
+    def Planning(self, animation=True):
+        """
+        Pathplanning
+        animation: flag for animation on or off
+        """
+
+        self.nodeList = [self.start]
+        for i in range(self.maxIter):
+            if(i%10==0):
+                print("Planning path",i)
+			
+            for e in pygame.event.get():
+                if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                    sys.exit("Exiting")
+
+            rnd = self.get_random_point()
+            nind = self.GetNearestListIndex(self.nodeList, rnd)
+
+            newNode = self.steer(rnd, nind)
+            if newNode is None:
+                continue
+
+            if self.CollisionCheck(newNode, self.obstacleList):
+                nearinds = self.find_near_nodes(newNode)
+                newNode = self.choose_parent(newNode, nearinds)
+                if newNode is None:
+                    continue
+                self.nodeList.append(newNode)
+                self.rewire(newNode, nearinds)
+
+            if animation and i % 5 == 0:
+                self.DrawGraph(rnd=rnd)
+            #if(newNode.)
+
+        # generate coruse
+        lastIndex = self.get_best_last_index()
+        if lastIndex is None:
+            return None
+        path = self.gen_final_course(lastIndex)
+        print("Path planning done")
+        return path
+
+    def choose_parent(self, newNode, nearinds):
+        if len(nearinds) == 0:
+            return newNode
+
+        dlist = []
+        for i in nearinds:
+            tNode = self.steer(newNode, i)
+            if tNode is None:
+                continue
+
+            if self.CollisionCheck(tNode, self.obstacleList):
+                dlist.append(tNode.cost)
+            else:
+                dlist.append(float("inf"))
+
+        mincost = min(dlist)
+        minind = nearinds[dlist.index(mincost)]
+
+        if mincost == float("inf"):
+            print("mincost is inf")
+            return newNode
+
+        newNode = self.steer(newNode, minind)
+
+        return newNode
+
+    def pi_2_pi(self, angle):
+        return (angle + math.pi) % (2 * math.pi) - math.pi
+
+    def steer(self, rnd, nind):
+
+        nearestNode = self.nodeList[nind]
+
+        px, py, pyaw, mode, clen = dubins_path_planning.dubins_path_planning(
+            nearestNode.x, nearestNode.y, nearestNode.yaw, rnd.x, rnd.y, rnd.yaw, curvature)
+
+        if px is None:
+            return None
+
+        newNode = copy.deepcopy(nearestNode)
+        newNode.x = px[-1]
+        newNode.y = py[-1]
+        newNode.yaw = pyaw[-1]
+
+        newNode.path_x = px
+        newNode.path_y = py
+        newNode.path_yaw = pyaw
+        newNode.cost += clen
+        newNode.parent = nind
+
+        return newNode
+
+    def get_random_point(self):
+
+        if random.randint(0, 100) > self.goalSampleRate:
+            rnd = [random.uniform(self.minrand, self.maxrand),
+                   random.uniform(self.minrand, self.maxrand),
+                   random.uniform(-math.pi, math.pi)
+                   ]
+        else:  # goal point sampling
+            rnd = [self.end.x, self.end.y, self.end.yaw]
+
+        node = Node(rnd[0], rnd[1], rnd[2])
+
+        return node
+
+    def get_best_last_index(self):
+        #  print("get_best_last_index")
+
+        YAWTH = np.deg2rad(3.0)
+        XYTH = 0.5
+
+        goalinds = []
+        for (i, node) in enumerate(self.nodeList):
+            if self.calc_dist_to_goal(node.x, node.y) <= XYTH:
+                goalinds.append(i)
+        #  print("OK XY TH num is")
+        #  print(len(goalinds))
+
+        # angle check
+        fgoalinds = []
+        for i in goalinds:
+            if abs(self.nodeList[i].yaw - self.end.yaw) <= YAWTH:
+                fgoalinds.append(i)
+        #  print("OK YAW TH num is")
+        #  print(len(fgoalinds))
+
+        if len(fgoalinds) == 0:
+            return None
+
+        mincost = min([self.nodeList[i].cost for i in fgoalinds])
+        for i in fgoalinds:
+            if self.nodeList[i].cost == mincost:
+                return i
+
+        return None
+
+    def gen_final_course(self, goalind):
+        path = [[self.end.x, self.end.y]]
+        while self.nodeList[goalind].parent is not None:
+            node = self.nodeList[goalind]
+            for (ix, iy) in zip(reversed(node.path_x), reversed(node.path_y)):
+                path.append([ix, iy])
+            #  path.append([node.x, node.y])
+            goalind = node.parent
+        path.append([self.start.x, self.start.y])
+        return path
+
+    def calc_dist_to_goal(self, x, y):
+        return np.linalg.norm([x - self.end.x, y - self.end.y])
+
+    def find_near_nodes(self, newNode):
+        nnode = len(self.nodeList)
+        r = 50.0 * math.sqrt((math.log(nnode) / nnode))
+        #  r = self.expandDis * 5.0
+        dlist = [(node.x - newNode.x) ** 2 +
+                 (node.y - newNode.y) ** 2 +
+                 (node.yaw - newNode.yaw) ** 2
+                 for node in self.nodeList]
+        nearinds = [dlist.index(i) for i in dlist if i <= r ** 2]
+        return nearinds
+
+    def rewire(self, newNode, nearinds):
+
+        nnode = len(self.nodeList)
+
+        for i in nearinds:
+            nearNode = self.nodeList[i]
+            tNode = self.steer(nearNode, nnode - 1)
+            if tNode is None:
+                continue
+
+            obstacleOK = self.CollisionCheck(tNode, self.obstacleList)
+            imporveCost = nearNode.cost > tNode.cost
+
+            if obstacleOK and imporveCost:
+                #  print("rewire")
+                self.nodeList[i] = tNode
+
+    def DrawGraph(self, rnd=None):
+        """
+        Draw Graph
+        """
+        reset()
+        if rnd is not None:
+            plt.plot(rnd.x, rnd.y, "^k")
+        for node in self.nodeList:
+            if node.parent is not None:
+                plt.plot(node.path_x, node.path_y, "-g")
+                for i in range(1, len(node.path_x)):
+                    pygame.draw.line(screen,blue,(node.path_x[i-1], node.path_y[i-1]), (node.path_x[i], node.path_y[i]))
+                #  plt.plot([node.x, self.nodeList[node.parent].x], [
+                #  node.y, self.nodeList[node.parent].y], "-g")
+
+        for (ox, oy, size) in self.obstacleList:
+            #plt.plot(ox, oy, "ok", ms=30 * size)
+            pygame.draw.circle(screen, black, (ox, oy), size)
+
+        '''reeds_shepp_path_planning.plot_arrow(self.start.x, self.start.y, self.start.yaw)
+        reeds_shepp_path_planning.plot_arrow(self.end.x, self.end.y, self.end.yaw)'''
+        pygame.draw.circle(screen, red, (self.start.x, self.start.y), GOAL_RADIUS)
+        pygame.draw.circle(screen, green, (self.end.x, self.end.y), GOAL_RADIUS)
+
+        '''plt.axis([-2, 15, -2, 15])
+        plt.grid(True)
+        plt.pause(0.01)'''
+        pygame.display.update()
+        #  plt.show()
+        #  input()
+
+    def GetNearestListIndex(self, nodeList, rnd):
+        dlist = [(node.x - rnd.x) ** 2 +
+                 (node.y - rnd.y) ** 2 +
+                 (node.yaw - rnd.yaw) ** 2 for node in nodeList]
+        minind = dlist.index(min(dlist))
+
+        return minind
+
+    def CollisionCheck(self, node, obstacleList):
+
+        for (ox, oy, size) in obstacleList:
+            for (ix, iy) in zip(node.path_x, node.path_y):
+                dx = ox - ix
+                dy = oy - iy
+                d = dx * dx + dy * dy
+                if d <= size ** 2:
+                    return False  # collision
+
+        return True  # safe
+
+
+class Node():
+    """
+    RRT Node
+    """
+
+    def __init__(self, x, y, yaw):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.path_x = []
+        self.path_y = []
+        self.path_yaw = []
+        self.cost = 0.0
+        self.parent = None
+
+
+
+
+def main():
+    global count
+    
+    initPoseSet = False
+    initialPoint = Node(None, None, 0)
+    goalPoseSet = False
+    goalPoint = Node(None, None, 0)
+    currentState = 'init'
+    rrt = None
+    path = None
+
+    nodes = []
+    reset()
+
+    while True:
+        if currentState == 'init':
+            pygame.display.set_caption('Select Starting Point and then Goal Point')
+            fpsClock.tick(10)
+        elif currentState == 'goalFound':
+            pygame.display.set_caption('Goal Reached')
+            rrt.DrawGraph()
+            for i in range(1, len(path)):
+                pygame.draw.line(screen, red, path[i-1], path[i])
+            optimizePhase = True
+        elif currentState == 'optimize':
+            fpsClock.tick(0.5)
+            pass
+        elif currentState == 'buildTree':
+            rrt = RRT((initialPoint.x, initialPoint.y, initialPoint.yaw), (goalPoint.x, goalPoint.y, goalPoint.yaw), randArea=[0, 500], obstacleList=obs)
+            path = rrt.Planning(True)
+            if(path != None):
+                currentState = 'goalFound'
+
+        for e in pygame.event.get():
+            if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                sys.exit("Exiting")
+            if e.type == MOUSEBUTTONDOWN:
+                print('mouse down')
+                if currentState == 'init':
+                    if initPoseSet == False:
+                        nodes = []
+                        if collides(e.pos) == False:
+                            print('initiale point set: '+str(e.pos))
+
+                            initialPoint = Node(e.pos[0], e.pos[1], np.deg2rad(0.0))
+                            initPoseSet = True
+                            pygame.draw.circle(screen, red, (initialPoint.x, initialPoint.y), GOAL_RADIUS)
+                    elif goalPoseSet == False:
+                        print('goal point set: '+str(e.pos))
+                        if collides(e.pos) == False:
+                            goalPoint = Node(e.pos[0], e.pos[1], np.deg2rad(0.0))
+                            goalPoseSet = True
+                            pygame.draw.circle(screen, green, (goalPoint.x, goalPoint.y), GOAL_RADIUS)
+                            #pygame.display.update()
+                            currentState = 'buildTree'
+                else:
+                    currentState = 'init'
+                    initPoseSet = False
+                    goalPoseSet = False
+                    reset()
+
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+
+
+if __name__ == '__main__':
+    main()
+    
